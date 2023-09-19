00df0fabf97e3ef9b97245b16aa8bb07b9dadf6e
Yangwon Park <ywonp94@gmail.com>
2023-09-19 13:25:57 +0900

    Project init.

---
+++ src/main/java/our/neighbor/NeighborApplication.java
@@ -0,0 +1,13 @@
+package our.neighbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/our/neighbor/NeighborApplicationTests.java
@@ -0,0 +1,13 @@
+package our.neighbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
